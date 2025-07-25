89c1c240d5f04cd1a686bf32e3a3ed5c020eca9e
Elena Pischurova <boombosha@gmail.com>
2025-07-25 16:55:44 +0300

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,40 @@
 export default defineNuxtConfig({
   srcDir: 'src/',
   devtools: { enabled: true },
+
   components: [
     { 
       path: '~/widgets',
       pathPrefix: false,
       global: true
-    }
+    },
+    
+    {
+      path: '~/features',
+      pathPrefix: false,
+      global: false
+    },
+    
+    '~/components'
   ],
+
   imports: {
     dirs: [
-      '~/widgets/**/components'
+      '~/widgets/**/components',
+      '~/features/**/api',
+      '~/features/**/lib'
     ]
   },
+
   css: [
     '@/app/styles/main.css'
-  ]
+  ],
+
+  runtimeConfig: {
+    public: {
+      apiBase: 'https://6082e3545dbd2c001757abf5.mockapi.io'
+    }
+  },
+
+  modules: ['@nuxt/image']
 })
